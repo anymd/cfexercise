f7cae7c5710b3789d2b2212fe0d897e65b026109
anymd <anymd-dev@yahoo.com>
2015-10-19 01:17:28 -0700

    refactor template js

--- app/assets/javascripts/templates.js
+++ app/assets/javascripts/templates.js
@@ -1,65 +1,53 @@
 $(document).ready(function(){
 
-  var templates = $('#templates').data('templates')
-
-  $.each(templates, function(index,val) {
-
+  var data = $('#templates').data('templates')
+  var initial_count = 8
+  
+  $.each(data, function(index,val) {
     var extra = '';
-    if (index > 7) {
-      extra = "extra-template"
-    } 
+    if (index > initial_count - 1) { extra = "extra-template" }
+
+    var image = '<img src="images/' + val.image + '">'
+    var name = '<div class="name">' + val.name + '</div>'
 
     $('#grid').append(
-      '<li class=' + extra + '><div class="template">' +
-        '<img src="images/' + val.image + '">' +
-        '<div class="name">' + val.name + '</div></div>' +
-      '</li>'
+      '<li class=' + extra + '><div class="template">' + image + name + '</div></li>'
     );
-
-  });
-
-  $('.more').click(function(){
-    $(this).hide();
-    $('.extra-template').css('display','inline-block');
-  });
-
-  $("#message_text").on('change keyup paste', function() {
-    $('.send').prop('disabled', false);
-    $('#message-response').hide();
-
-    if (($(this).val().length == 0) || ($(this).val().length > 140)) {
-      $('.send').prop('disabled', true);
-    }
-
-    if ($(this).val().length > 140) {
-      $('#message_error').text('Your message must be 140 characters or fewer');
-    }
-    else {
-      $('#message_error').text('');
-    }
-
-    if ($('.send').prop('disabled') == true) {
-      $( ".send" ).addClass('disabled'); 
-    } else {
-      $( ".send" ).removeClass('disabled'); 
-    }
-
-  });
-
-  $('.send-another-message').click(function(){
-    $('#message_text').val('')
-    $('#success_message').hide();
-    $('#new_message').show();
   });
 
   $('.extra-template').hide();
 
-  $('input,textarea').focus(function(){
-     $(this).data('placeholder',$(this).attr('placeholder'))
-            .attr('placeholder','');
-  }).blur(function(){
-     $(this).attr('placeholder',$(this).data('placeholder'));
-  });  
+  $('.more').click(function(){
+    $(this).hide();
+    $('.extra-template').show();
+  });
+
+  $("#message_text").on('change keyup paste', function() {
+    reset(); length = $(this).val().length;
+
+    if (length == 0 || length > 140) { buttonStatus(true); }
+    if (length > 140) { messageError('Your message must be 140 characters or fewer'); }
+
+  });
+
+  function reset() {
+    buttonStatus(false);
+    messageError('');
+    $('#message-response').hide();
+  }
+
+  function buttonStatus(state) {
+    $('.send').prop('disabled', state);
+    if (state) {
+      $(".send").addClass('disabled');
+    } else {
+      $(".send").removeClass('disabled');
+    }
+  }
+
+  function messageError(str) {
+    $('#message_error').text(str)
+  }
 
 });
 
